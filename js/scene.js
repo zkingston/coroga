842777fb7a2565915f5eab33a2d5fd858490984b
zkingston <kingston.zak@gmail.com>
2015-11-30 14:48:50 -0600

    More testing of demos. Added orbitcontrols.

--- js/scene.js
+++ js/scene.js
@@ -1,24 +1,86 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var container, stats;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+var camera, controls, scene, renderer;
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var cross;
 
-camera.position.z = 5;
-
-var render = function () {
-    requestAnimationFrame( render );
-
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
-
-    renderer.render(scene, camera);
-};
-
+init();
 render();
+animate();
+
+function init() {
+
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera.position.z = 500;
+
+    controls = new THREE.OrbitControls( camera );
+    controls.addEventListener( 'change', render );
+
+    scene = new THREE.Scene();
+    scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+
+    // world
+
+    var geometry = new THREE.CylinderGeometry( 0, 10, 30, 4, 1 );
+    var material = new THREE.MeshLambertMaterial( { color:0xffffff } );
+
+    for ( var i = 0; i < 50; i ++ ) {
+
+        var mesh = new THREE.Mesh( geometry, material );
+        mesh.position.x = ( Math.random() - 0.5 ) * 500;
+        mesh.position.y = ( Math.random() - 0.5 ) * 500;
+        mesh.position.z = ( Math.random() - 0.5 ) * 500;
+        mesh.updateMatrix();
+        mesh.matrixAutoUpdate = false;
+        scene.add( mesh );
+
+    }
+
+
+    // lights
+
+    light = new THREE.DirectionalLight( 0xffffff );
+    light.position.set( 1, 1, 1 );
+    scene.add( light );
+
+    light = new THREE.DirectionalLight( 0x002288 );
+    light.position.set( -1, -1, -1 );
+    scene.add( light );
+
+    light = new THREE.AmbientLight( 0x222222 );
+    scene.add( light );
+
+
+    // renderer
+
+    renderer = new THREE.WebGLRenderer( { antialias: false } );
+    renderer.setClearColor( scene.fog.color, 1 );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    document.body.appendChild( renderer.domElement );
+
+    window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    render();
+
+}
+
+function animate() {
+
+    requestAnimationFrame( animate );
+    controls.update();
+
+}
+
+function render() {
+    renderer.render( scene, camera );
+}
