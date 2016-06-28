30c37d9e2153458c3622c73ead64c0a6783464f4
Zak Kingston <kingston.zak@gmail.com>
2016-06-28 12:36:23 -0500

    Better alert / button justification

--- js/util.js
+++ js/util.js
@@ -11,17 +11,17 @@ String.prototype.format = function() {
 };
 
 function alertSuccess( text ) {
-    document.getElementById( 'alerts' ).innerHTML = alert.format( 'success', 'Success', text );
+    document.getElementById( 'alerts' ).innerHTML += alert.format( 'success', 'Success', text );
 }
 
 function alertInfo( text ) {
-    document.getElementById( 'alerts' ).innerHTML = alert.format( 'info', 'Info', text );
+    document.getElementById( 'alerts' ).innerHTML += alert.format( 'info', 'Info', text );
 }
 
 function alertWarning( text ) {
-    document.getElementById( 'alerts' ).innerHTML = alert.format( 'warning', 'Warning', text );
+    document.getElementById( 'alerts' ).innerHTML += alert.format( 'warning', 'Warning', text );
 }
 
 function alertError( text ) {
-    document.getElementById( 'alerts' ).innerHTML = alert.format( 'danger', 'Error', text );
+    document.getElementById( 'alerts' ).innerHTML += alert.format( 'danger', 'Error', text );
 }
