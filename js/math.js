b599c541f0750e96d38a3a5de349366d2b657f7b
Ayush Narayan <asn2@rice.edu>
2016-07-01 01:32:15 -0500

    Commented trees... a lot. Replaced flowers with particles

--- js/math.js
+++ js/math.js
@@ -1,11 +1,33 @@
 
-
+acos = Math.acos
+tan = Math.tan
 cos = Math.cos;
 sin = Math.sin;
 random = Math.random;
 ln = Math.log;
 pi = Math.PI;
 sqrt = Math.sqrt;
+
+
+/**
+* Bernoulli distribution. Returns true with probability n
+* @param {number} n The probability true is returned
+* @return {boolean} a boolean.
+**/
+function bernoulli(n){
+    return  (random() < n) ? true : false;
+}
+
+/**
+* Uniform distribution Returns a random number within two bounds
+* @param {number} low The lower bound a random number is produced
+* @param {number} high The upper bound a random number is produced
+* @return {number} A random number
+**/
+function uniform(low, high){
+    return Math.random()*(high -low) + low
+}
+
 
 /**
 * Normalizes a dirichlet vector represented by an array
@@ -29,8 +51,6 @@ function dirichletNormalize(x){
 * @return {number} the index of the array that was chosen
 *
 **/
-
-
 function dirichletSample(x) {
     if(x.length == 0){
         console.log("Error in Dirichlet sampling in file math.js");
